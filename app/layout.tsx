2a0480c99343bc3efc395bc5a20b0a2bfff7e2d4
Eric Chang <ericchang@Erics-MacBook-Air-5.local>
2026-08-09 09:12:48 -0400

    redesign

--- app/layout.tsx
+++ app/layout.tsx
@@ -107,7 +107,7 @@ export const metadata: Metadata = {
   },
   icons: {
     icon: [
-      { url: "/icon.png", type: "image/png" },
+      { url: "/favicon.png", type: "image/png" },
       { url: "/icon-192.png", sizes: "192x192", type: "image/png" },
       { url: "/icon-512.png", sizes: "512x512", type: "image/png" },
     ],
